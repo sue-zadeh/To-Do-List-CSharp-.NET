ea1a414a0140879eb91ac258a6e70d6f6b78715b
Sue Zadeh <suezadeh.a@gmail.com>
2024-06-30 19:24:51 +1200

    index

--- obj/Debug/net8.0/practice-on-.net.AssemblyInfo.cs
+++ obj/Debug/net8.0/practice-on-.net.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice-on-.net")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c49aa179784852afeae743f663487081d42eeffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2666dd2387c0c7f987c50581ee92f3b52241f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice-on-.net")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice-on-.net")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
